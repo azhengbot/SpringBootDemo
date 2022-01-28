7749c0d0d4e601646ec503c746d8be4564c0db0d
xuhaoqing <xuhaoqing@lattebank.com>
2022-01-28 18:08:07 +0800

    add: bill

--- src/main/java/com/example/springboot_demo/controller/BillController.java
+++ src/main/java/com/example/springboot_demo/controller/BillController.java
@@ -3,7 +3,6 @@ package com.example.springboot_demo.controller;
 import com.example.springboot_demo.domain.bill.BillRecord;
 import com.example.springboot_demo.service.BillService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
